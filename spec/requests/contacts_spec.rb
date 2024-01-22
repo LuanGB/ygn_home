f8d4fba1c6a0f045c6135dc13a27047c6602b632
LuanGB <luan.goncbs@gmail.com>
2024-01-22 18:16:32 -0300

    fix(): tests

--- spec/requests/contacts_spec.rb
+++ spec/requests/contacts_spec.rb
@@ -26,47 +26,16 @@ RSpec.describe '/contacts' do
     skip('Add a hash of attributes invalid for your model')
   end
 
-  describe 'GET /index' do
-    it 'renders a successful response' do
-      Contact.create! valid_attributes
-      get contacts_url
-      expect(response.successful?).to be(true)
-    end
-  end
-
-  describe 'GET /show' do
-    it 'renders a successful response' do
-      contact = Contact.create! valid_attributes
-      get contact_url(contact)
-      expect(response.successful?).to be(true)
-    end
-  end
-
-  describe 'GET /new' do
-    it 'renders a successful response' do
-      get new_contact_url
-      expect(response.successful?).to be(true)
-    end
-  end
-
-  describe 'GET /edit' do
-    it 'renders a successful response' do
-      contact = Contact.create! valid_attributes
-      get edit_contact_url(contact)
-      expect(response.successful?).to be(true)
-    end
-  end
-
   describe 'POST /create' do
     context 'with valid parameters' do
       it 'creates a new Contact' do
         expect do
-          post contacts_url, params: { contact: valid_attributes }
+          post api_v1_contacts_url, params: { contact: valid_attributes }
         end.to change(Contact, :count).by(1)
       end
 
       it 'redirects to the created contact' do
-        post contacts_url, params: { contact: valid_attributes }
+        post api_v1_contacts_url, params: { contact: valid_attributes }
         expect(response).to redirect_to(contact_url(Contact.last))
       end
     end
@@ -74,59 +43,14 @@ RSpec.describe '/contacts' do
     context 'with invalid parameters' do
       it 'does not create a new Contact' do
         expect do
-          post contacts_url, params: { contact: invalid_attributes }
+          post api_v1_contacts_url, params: { contact: invalid_attributes }
         end.not_to change(Contact, :count)
       end
 
       it "renders a response with 422 status (i.e. to display the 'new' template)" do
-        post contacts_url, params: { contact: invalid_attributes }
+        post api_v1_contacts_url, params: { contact: invalid_attributes }
         expect(response.has_http_status?(:unprocessable_entity)).to be(true)
       end
-    end
-  end
-
-  describe 'PATCH /update' do
-    context 'with valid parameters' do
-      let(:new_attributes) do
-        skip('Add a hash of attributes valid for your model')
-      end
-
-      it 'updates the requested contact' do
-        contact = Contact.create! valid_attributes
-        patch contact_url(contact), params: { contact: new_attributes }
-        contact.reload
-        skip('Add assertions for updated state')
-      end
-
-      it 'redirects to the contact' do
-        contact = Contact.create! valid_attributes
-        patch contact_url(contact), params: { contact: new_attributes }
-        contact.reload
-        expect(response).to redirect_to(contact_url(contact))
-      end
-    end
-
-    context 'with invalid parameters' do
-      it "renders a response with 422 status (i.e. to display the 'edit' template)" do
-        contact = Contact.create! valid_attributes
-        patch contact_url(contact), params: { contact: invalid_attributes }
-        expect(response.has_http_status?(:unprocessable_entity)).to be(true)
-      end
-    end
-  end
-
-  describe 'DELETE /destroy' do
-    it 'destroys the requested contact' do
-      contact = Contact.create! valid_attributes
-      expect do
-        delete contact_url(contact)
-      end.to change(Contact, :count).by(-1)
-    end
-
-    it 'redirects to the contacts list' do
-      contact = Contact.create! valid_attributes
-      delete contact_url(contact)
-      expect(response).to redirect_to(contacts_url)
     end
   end
 end
