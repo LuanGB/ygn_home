70f5e8bea3689282525b864f56e3f51540fad726
LuanGB <luan.goncbs@gmail.com>
2024-02-29 21:32:54 -0300

    feat(): blog improvements

--- app/controllers/blog/categories_controller.rb
+++ app/controllers/blog/categories_controller.rb
@@ -1,19 +1,30 @@
 # frozen_string_literal: true
 
 module Blog
-  class CategoriesController < ApplicationController
+  class CategoriesController < BlogController
     before_action :set_blog_category, only: :show
 
     # GET /blog/categories or /blog/categories.json
     def index
       @blog_categories = Blog::Category.all
+      add_breadcrumb('Blog', '/blog')
+      add_page_config(:page_title, "Categories")
+    end
+
+    def show
+      @blog_posts = @blog_category.posts.order(published_at: :desc).page(params.fetch(:page, 1)).per(10)
+
+      add_breadcrumb('Blog', '/blog')
+      add_page_config(:page_title, "\"#{params[:id]}\" Posts")
+
+      render "blog/posts/index", layout: 'blog'
     end
 
     private
 
     # Use callbacks to share common setup or constraints between actions.
     def set_blog_category
-      @blog_category = Blog::Category.find(params[:id])
+      @blog_category = Blog::Category.find_by(name: params[:id])
     end
 
     # Only allow a list of trusted parameters through.
--- app/controllers/blog/posts_controller.rb
+++ app/controllers/blog/posts_controller.rb
@@ -6,13 +6,15 @@ module Blog
     def index
       scope = Blog::Post.published
       scope = scope.where('title like :search_term or content like :search_term', search_term: "%#{params[:search]}%") if params[:search].present?
-      scope = scope.where('title like :search_term or content like :search_term', search_term: "%#{params[:search]}%") if params[:search].present?
-      scope = scope.where('title like :search_term or content like :search_term', search_term: "%#{params[:search]}%") if params[:search].present?
-      @blog_posts.order(published_at: :desc).page(params.fetch(:page, 1)).per(10)
+      scope = scope.joins(:categories).where('blog_categories.name' => params[:categories].map(&:upcase)) if params[:categories].present?
+      scope = scope.joins(:tags).where('blog_tags.name' => params[:tags].map(&:downcase)) if params[:tags].present?
+      @blog_posts = scope.order(published_at: :desc).page(params.fetch(:page, 1)).per(10)
 
-      if params[:search].present? || params[:category].present? || params[:tag].present?
+      if params[:search].present? || params[:categories].present? || params[:tags].present?
         add_breadcrumb('Blog', '/blog')
-        add_page_config(:page_title, "# Search results for: \"#{params[:search]}\"")
+        add_page_config(:page_title, "# Posts for #{params[:tags].join(' & ')}") if params[:tags].present?
+        add_page_config(:page_title, "# Posts for #{params[:categories].join(' & ')}") if params[:categories].present?
+        add_page_config(:page_title, "# Search results for: \"#{params[:search]}\"") if params[:search].present?
       end
     end
 
